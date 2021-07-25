6f6cca7636cdb34537ea5695152f696215ba35ff
Evan Archuleta <Evan.archuleta02@gmail.com>
2021-07-24 21:09:59 -0600

    Feat: Filter tickers by 200 day MA

--- df_creation.py
+++ df_creation.py
@@ -29,14 +29,32 @@ print(df2.shape)
 df2.rename( columns={'Unnamed: 0':'Date'}, inplace=True )
 df2.set_index(keys="Date", inplace = True)
 
-## historical 90 day vol 
+## historical 90 day vol -- check for accuracy https://www.etfreplay.com/volatility.aspx
 
 returns = df2.pct_change()
 df_returns = returns.dropna()
 df_returns = df_returns.tail(64)
-
 hist_vol = (df_returns.std())*(252**.5)
 print(hist_vol)
 
-print(df_returns.shape)
-print(df_returns.head())
+## 200 day moving average  = 142 bus days 
+
+df_ma = df2.tail(142)
+ma_200 = df_ma.mean()
+
+print(ma_200)
+
+ma_df = pd.DataFrame(ma_200, columns=["ma"])
+ma_df2 = ma_df.T
+ma_df2 = ma_df2.append(df2.tail(1))
+
+ma_df2.loc['Diff'] = ma_df2.loc['2021-07-23'] - ma_df2.loc['ma'] 
+ma_df2.loc['Diff %'] = (ma_df2.loc['2021-07-23'] / ma_df2.loc['ma']) -1
+
+df_filtered = ma_df2.T
+
+print(df_filtered)
+
+df_filtered = df_filtered[df_filtered['Diff %'] > 0 ]
+sort = df_filtered['Diff %'].sort_values(ascending=False)
+print(sort)

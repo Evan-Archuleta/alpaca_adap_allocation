51e3dca33b6f499a76fb99d66337214cce797f38
Evan Archuleta <Evan.archuleta02@gmail.com>
2022-05-16 15:18:05 -0600

    updated explore to see market value

--- explore.py
+++ explore.py
@@ -18,15 +18,14 @@ api = tradeapi.REST(APIKEYID, APISECRETKEY, APIBASEURL)
 # print(type(have('TMF')))
 # print(have('TMF'))
 
-# check print the quantity of shares for each position.
-# portfolio = api.list_positions()
-# for position in portfolio:
-#     print("{} shares of {}".format(position.qty, position.symbol))
+## check print the quantity of shares for each position.
+portfolio = api.list_positions()
+for position in portfolio:
+    print("{} shares of {}".format(position.qty, position.symbol))
 
 account_value = api.get_account()
 account_value = float(account_value.last_equity)
-print("Avalible to trade:")
-print(account_value)
+print("Avalible to trade:", account_value)
 
 portfolio = api.list_positions()
 market_value = []
@@ -36,20 +35,21 @@ for position in portfolio:
 market_value = sum(market_value)
 
 
-print("Market Value:")
-print(market_value) 
+print("Market Value:", market_value)
+print("Portfolio Value:", (market_value+account_value))
 
 
-for position in portfolio:
-    print("{} shares of {}".format(position.qty, position.symbol))
 
-print('test')
-print(portfolio)   
+# for position in portfolio:
+#     print("{} shares of {}".format(position.qty, position.symbol))
 
-print('test_real')
-tmf_position = api.get_position('TMF')
-#print("test2")
-print(tmf_position.qty)
-# export df to csv 
-#print(research)
-#research.to_csv('explore.csv', header=True)
+# print('test')
+# print(portfolio)   
+
+# print('test_real')
+# tmf_position = api.get_position('TMF')
+# #print("test2")
+# print(tmf_position.qty)
+# # export df to csv 
+# #print(research)
+# #research.to_csv('explore.csv', header=True)
